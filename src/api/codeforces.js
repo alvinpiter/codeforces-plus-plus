c63223259445fd4c1894b4a7d55ef807ed5b45d8
Alvin Piter <alvin.piter@bukalapak.com>
2020-08-13 13:13:09 +0800

    Implement http client (#1)
    * Implement getUserSubmissions
    
    * Implement getProblemsetProblems
    
    * Shorten code

---
+++ src/api/codeforces.js
@@ -0,0 +1,36 @@
+import { get } from './client.js'
+
+const codeforcesBaseURL = 'https://codeforces.com/api'
+
+function problemMapper(problem) {
+  return {
+    id: `${problem.contestId}${problem.index}`,
+    name: problem.name,
+    rating: problem.rating,
+    tags: problem.tags
+  }
+}
+
+function submissionMapper(submission) {
+  return {
+    id: submission.id,
+    verdict: submission.verdict,
+    problem: problemMapper(submission.problem)
+  }
+}
+
+export async function getProblemsetProblems() {
+  const url = `${codeforcesBaseURL}/problemset.problems`
+  const jsonResponse = await get(url)
+  const problems = jsonResponse.result.problems.map(problemMapper)
+
+  return problems
+}
+
+export async function getUserSubmissions(handle) {
+  const url = `${codeforcesBaseURL}/user.status?handle=${handle}`
+  const jsonResponse = await get(url)
+  const submissions = jsonResponse.result.map(submissionMapper)
+
+  return submissions
+}
