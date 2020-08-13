d15ac1ccefcb2303b9247e282cae84f51fee1904
Alvin Piter <alpiter10@gmail.com>
2020-08-13 16:36:52 +0800

    Filter problems by tags

--- src/features/filterProblems.js
+++ src/features/filterProblems.js
@@ -12,6 +12,13 @@ contestID: {
   minimum: 500,
   maximum: 750
 }
+
+* filter by tags. Format:
+tags: {
+  mode: "AND"/"OR",
+  tags: ["implementation", "greedy"]
+}
+
 */
 export function filterProblems(problems, filterParameters) {
   const filterFunction = buildFilterFunction(filterParameters)
@@ -30,6 +37,10 @@ function buildFilterFunction(filterParameters) {
       ok = ok && filterByContestID(problem, filterParameters.contestID)
     }
 
+    if (filterParameters.hasOwnProperty('tags')) {
+      ok = ok && filterByTags(problem, filterParameters.tags)
+    }
+
     return ok
   }
 }
@@ -46,4 +57,32 @@ function filterByContestID(problem, contestIDFilter) {
   const maxContestID = contestIDFilter.maximum
 
   return (problem.contestID >= minContestID && problem.contestID <= maxContestID)
+}
+
+function filterByTags(problem, tagsFilter) {
+  if (tagsFilter.mode === "AND") {
+    return filterByTagsWithAND(problem, tagsFilter.tags)
+  } else {
+    return filterByTagsWithOR(problem, tagsFilter.tags)
+  }
+}
+
+function filterByTagsWithAND(problem, tags) {
+  let problemTagsSet = new Set(problem.tags)
+  for (let tag of tags) {
+    if (!problemTagsSet.has(tag))
+      return false
+  }
+
+  return true
+}
+
+function filterByTagsWithOR(problem, tags) {
+  let tagsSet = new Set(tags)
+  for (let tag of problem.tags) {
+    if (tagsSet.has(tag))
+      return true
+  }
+
+  return false
 }
