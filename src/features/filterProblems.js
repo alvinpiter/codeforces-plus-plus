ed45d531598fc7dad947cc35bf4a2f44e97928c7
Alvin Piter <alpiter10@gmail.com>
2020-08-13 16:03:08 +0800

    Filter by contest ID

--- src/features/filterProblems.js
+++ src/features/filterProblems.js
@@ -6,6 +6,12 @@ rating: {
   minimum: 1000,
   maximum: 2000
 }
+
+* filter by contest ID. Format:
+contestID: {
+  minimum: 500,
+  maximum: 750
+}
 */
 export function filterProblems(problems, filterParameters) {
   const filterFunction = buildFilterFunction(filterParameters)
@@ -20,6 +26,10 @@ function buildFilterFunction(filterParameters) {
       ok = ok && filterByRating(problem, filterParameters.rating)
     }
 
+    if (filterParameters.hasOwnProperty('contestID')) {
+      ok = ok && filterByContestID(problem, filterParameters.contestID)
+    }
+
     return ok
   }
 }
@@ -29,4 +39,11 @@ function filterByRating(problem, ratingFilter) {
   const maxRating = ratingFilter.maximum
 
   return (problem.rating >= minRating && problem.rating <= maxRating)
+}
+
+function filterByContestID(problem, contestIDFilter) {
+  const minContestID = contestIDFilter.minimum
+  const maxContestID = contestIDFilter.maximum
+
+  return (problem.contestID >= minContestID && problem.contestID <= maxContestID)
 }
