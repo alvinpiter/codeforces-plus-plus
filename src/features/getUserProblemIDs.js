0d5f713f5e1133ff133ff47ccde01377d279a10f
Alvin Piter <alpiter10@gmail.com>
2020-08-20 20:31:56 +0800

    Remove unused code

--- src/features/getUserProblemIDs.js
+++ src/features/getUserProblemIDs.js
@@ -1,4 +1,3 @@
-import { filterProblems } from './filterProblems'
 import { getNormalizedSubmissions } from './getNormalizedSubmissions'
 import { filterNormalizedSubmissions } from './filterNormalizedSubmissions'
 
@@ -6,23 +5,5 @@ export async function getUserProblemIDs(handle, problems) {
   const submissions = await getNormalizedSubmissions(handle, problems)
   const solvedAndAttemptedProblemIDs = filterNormalizedSubmissions(submissions)
 
-  const solvedProblemIDs = solvedAndAttemptedProblemIDs.solvedProblemIDs.map(temp => temp.id)
-  const attemptedProblemIDs = solvedAndAttemptedProblemIDs.attemptedProblemIDs.map(temp => temp.id)
-
-  const notAttemptedProblems = filterProblems(problems, {
-    ids: {
-      mode: "EXCLUDE",
-      ids: solvedProblemIDs.concat(attemptedProblemIDs)
-    }
-  })
-
-  return {
-    ...solvedAndAttemptedProblemIDs,
-    notAttemptedProblemIDs: notAttemptedProblems.map(p => {
-      return {
-        id: p.id,
-        submittedID: null
-      }
-    })
-  }
+  return solvedAndAttemptedProblemIDs
 }
