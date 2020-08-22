fea54cdb28f3241710c471c4d003d9a4552de2b9
Alvin Piter <alpiter10@gmail.com>
2020-08-22 13:36:15 +0800

    Use ContestPicker in StandingsPage

--- src/components/StandingsPage.js
+++ src/components/StandingsPage.js
@@ -1,16 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import ContestPicker from './ContestPicker'
 import CircularProgress from '@material-ui/core/CircularProgress'
-import { getContestList } from '../api/codeforces'
+import { getPastContestList } from '../features/getPastContestList'
+import { aggregateStandings } from '../features/aggregateStandings'
+import StandingTable from './StandingTable'
 
 export default function StandingsPage() {
-  let [isLoading, setIsLoading] = useState(true)
+  let [isLoadingContests, setIsLoadingContests] = useState(true)
   let [contests, setContests] = useState([])
 
+  let [isLoadingStandings, setIsLoadingStandings] = useState(false)
+  let [standings, setStandings] = useState(null)
+
+  const onPicked = (contest) => {
+    setIsLoadingStandings(true)
+
+    const loadStandings = async (contest) => {
+      const standings = await aggregateStandings(contest.id)
+      setIsLoadingStandings(false)
+      setStandings(standings)
+    }
+
+    loadStandings(contest)
+  }
+
   useEffect(() => {
-    let prom = getContestList()
+    let prom = getPastContestList()
     prom.then(contests => {
-      setIsLoading(false)
+      setIsLoadingContests(false)
       setContests(contests)
     })
   }, [])
@@ -18,9 +35,14 @@ export default function StandingsPage() {
   return (
     <div>
       {
-        isLoading ?
+        isLoadingContests ?
         <CircularProgress /> :
-        <ContestPicker contests={contests} />
+        <ContestPicker contests={contests} onPicked={onPicked}/>
+      }
+      {
+        isLoadingStandings ?
+        <CircularProgress /> :
+        <StandingTable standings={standings} />
       }
     </div>
   )
