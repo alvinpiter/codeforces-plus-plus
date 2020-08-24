850268bb64e22396ff3888373278aae09c703657
Alvin Piter <alpiter10@gmail.com>
2020-08-24 10:20:40 +0800

    Overwrite some country names

--- src/utils/MyCountryList.js
+++ src/utils/MyCountryList.js
@@ -1,5 +1,28 @@
 import { overwrite, getCode } from 'country-list'
 
 export function getCountryCode(countryName) {
+  const nameCodePairs = [
+    ["United States", "US"],
+    ["Taiwan", "TW"],
+    ["South Korea", "KR"],
+    ["Vietnam", "VN"],
+    ["Russia", "RU"],
+    ["Iran", "IR"],
+    ["United Kingdom", "GB"],
+    ["Moldova", "MD"],
+    ["Syria", "SY"],
+    ["Macedonia", "MK"],
+    ["Bolivia", "BO"],
+    ["Palestinian Territory", "PS"],
+    ["North Korea", "KP"],
+    ["Serbia and Montenegro", "RS"]
+  ]
+
+  const nameCodeObjects = nameCodePairs.map(pair => {
+    return { name: pair[0], code: pair[1] }
+  })
+
+  overwrite(nameCodeObjects)
+
   return getCode(countryName)
 }
