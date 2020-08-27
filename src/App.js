eea2141c1f5b4116934d58aedd24c4162d6cbf5b
Alvin Piter <alpiter10@gmail.com>
2020-08-27 17:09:16 +0800

    Re-implement compare

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import ProblemsPage from './components/ProblemsPage'
+import { compare } from './features/compare'
 
 function App() {
+  let temp = compare('alvinpiter', 'jauhar.wibisono')
+  temp.then(res => console.log(res))
 
   return (
     <div>
