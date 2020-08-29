271db2f4d17346d79a6f5fd1ce4db4c7a17e9d7e
Alvin Piter <alpiter10@gmail.com>
2020-08-29 18:53:24 +0800

    Handle and country filter should be done with OR

--- src/features/filterRanklistRows.js
+++ src/features/filterRanklistRows.js
@@ -2,45 +2,29 @@
 /*
 Supported filterParameters:
 {
+  handles: ["alvinpiter", "tourist"],
   countryCodes: ["ID", "JP"]
 }
 */
 export function filterRanklistRows(rows, filterParameters) {
-  if (filterParameters.hasOwnProperty('handles'))
-    rows = filterRanklistRowsByHandles(rows, filterParameters.handles)
-
-  if (filterParameters.hasOwnProperty('countryCodes'))
-    rows = filterRanklistRowsByCountryCodes(rows, filterParameters.countryCodes)
-
-  return rows
-}
-
-function filterRanklistRowsByHandles(rows, handles) {
   let handlesSet = new Set()
-  for (let handle of handles)
-    handlesSet.add(handle)
+  if (filterParameters.hasOwnProperty('handles')) {
+    for (let handle of filterParameters.handles)
+      handlesSet.add(handle)
+  }
+
+  let countryCodesSet = new Set()
+  if (filterParameters.hasOwnProperty('countryCodes')) {
+    for (let code of filterParameters.countryCodes)
+      countryCodesSet.add(code)
+  }
 
   return rows.filter(row => {
     if (row.hasOwnProperty('userInfos')) {
       for (let userInfo of row.userInfos) {
         if (handlesSet.has(userInfo.handle))
           return true
-      }
 
-      return false
-    } else
-      return false
-  })
-}
-
-function filterRanklistRowsByCountryCodes(rows, countryCodes) {
-  let countryCodesSet = new Set()
-  for (let code of countryCodes)
-    countryCodesSet.add(code)
-
-  return rows.filter(row => {
-    if (row.hasOwnProperty('userInfos')) {
-      for (let userInfo of row.userInfos) {
         if (countryCodesSet.has(userInfo.countryCode))
           return true
       }
