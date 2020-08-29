715f02588c1c8901a5e316026b9392451be7a866
Alvin Piter <alpiter10@gmail.com>
2020-08-29 18:11:07 +0800

    Display handle/name autocomplete

--- src/components/StandingFilter.js
+++ src/components/StandingFilter.js
@@ -4,10 +4,23 @@ import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 
 export default function StandingFilter(props) {
-  const { countries, onCountriesChange, onApplyFilter } = props
+  const { countries, users, onCountriesChange, onUsersChange, onApplyFilter } = props
 
   return (
     <div>
+      <Autocomplete
+        multiple
+        options={users}
+        getOptionLabel={opt => opt.name === "" ? opt.handle : `${opt.handle} (${opt.name})`}
+        renderInput={params =>
+          <TextField
+            {...params}
+            variant="standard"
+            placeholder="Handles or names"
+          />
+        }
+        onChange={onUsersChange}
+      />
       <Autocomplete
         multiple
         options={countries}
