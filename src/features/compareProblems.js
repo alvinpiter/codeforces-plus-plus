4774ff7202e700fcbae48d3da54a063670ee281c
Alvin Piter <alpiter10@gmail.com>
2020-09-10 10:53:50 +0800

    Re-implement compareProblems

---
+++ src/features/compareProblems.js
@@ -0,0 +1,27 @@
+import { filterProblems } from './filterProblems'
+import { getPersonalizedProblems } from './getPersonalizedProblems'
+
+//Returns an array of problems (with metadata) solved by rivalHandle but not by handle
+export async function compareProblems(handle, rivalHandle) {
+  const problems = await getPersonalizedProblems(handle)
+  const rivalProblems = await getPersonalizedProblems(rivalHandle)
+
+  const solvedProblems = filterProblems(problems, { state: 1 })
+  const rivalSolvedProblems = filterProblems(rivalProblems, { state: 1 })
+
+  const diffProblems = filterProblems(rivalSolvedProblems, {
+    ids: {
+      mode: 'EXCLUDE',
+      ids: solvedProblems.map(p => p.id)
+    }
+  })
+
+  const result = filterProblems(problems, {
+    ids: {
+      mode: 'INCLUDE',
+      ids: diffProblems.map(p => p.id)
+    }
+  })
+
+  return result
+}
